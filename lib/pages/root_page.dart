cbe7db730d6c037d08e66c74d9178d34cafff985
Keith <kbacalso@guerilla360.com>
2023-01-09 18:04:25 +0800

    Removed unecessary bloc listener.

--- lib/pages/root_page.dart
+++ lib/pages/root_page.dart
@@ -32,33 +32,28 @@ class _RootPageState extends State<RootPage> {
     if (_selectedIndex == 1) _appbarTitle = 'Favorites';
     if (_selectedIndex == 2) _appbarTitle = 'Profile';
 
-    return BlocListener<UserCubit, UserState>(
-      listener: (context, state) {
-        context.goNamed(PAGE.viewItem.name);
-      },
-      child: Scaffold(
-        appBar: AppBar(title: Text(_appbarTitle)),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _selectedIndex,
-          onTap: (index) {
-            setState(() {
-              _selectedIndex = index;
+    return Scaffold(
+      appBar: AppBar(title: Text(_appbarTitle)),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
 
-              //* === UNCOMMENT CODE TO TRY ===
-              //* These 3 will not work if there path are not in GoRouter's routes.
-              //* These 3 will still work in mobile if removed because there is setState() _selectedIndex.
-              //* Web will still work if removed, but it is recommended because you can see the full path updating.
-              if (_selectedIndex == 0) context.go(PAGE.home.path);
-              if (_selectedIndex == 1) context.go(PAGE.favorites.path);
-              if (_selectedIndex == 2) context.go(PAGE.profile.path);
-            });
-          },
-          items: bottomNavBarItems,
-        ),
-        body: IndexedStack(
-          index: _selectedIndex,
-          children: const [HomePage(), FavoritesPage(), ProfilePage()],
-        ),
+            //* === UNCOMMENT CODE TO TRY ===
+            //* These 3 will not work if there path are not in GoRouter's routes.
+            //* These 3 will still work in mobile if removed because there is setState() _selectedIndex.
+            //* Web will still work if removed, but it is recommended because you can see the full path updating.
+            if (_selectedIndex == 0) context.go(PAGE.home.path);
+            if (_selectedIndex == 1) context.go(PAGE.favorites.path);
+            if (_selectedIndex == 2) context.go(PAGE.profile.path);
+          });
+        },
+        items: bottomNavBarItems,
+      ),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: const [HomePage(), FavoritesPage(), ProfilePage()],
       ),
     );
   }
